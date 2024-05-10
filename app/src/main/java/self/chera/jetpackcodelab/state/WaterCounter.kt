42aec4daed58d67609b38fcef634632aa5a6a475
keychera <kyogatama@gmail.com>
2024-05-10 11:59:10 +0900

    remember (step 7)

--- app/src/main/java/self/chera/jetpackcodelab/state/WaterCounter.kt
+++ app/src/main/java/self/chera/jetpackcodelab/state/WaterCounter.kt
@@ -1,12 +1,14 @@
-package self.chera.jetpackcodelab
+package self.chera.jetpackcodelab.state
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -23,12 +25,25 @@ fun WellnessScreen(modifier: Modifier = Modifier) {
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     var count by remember { mutableIntStateOf(0) }
+
     Column(modifier = modifier.padding(16.dp)) {
-        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
-            Text("Add one")
-        }
         if (count > 0) {
+            var showTask by remember { mutableStateOf(true) }
+            if (showTask) {
+                WellnessTaskItem(
+                    onClose = { showTask = false },
+                    taskName = "Have you taken your 15 minute walk today?"
+                )
+            }
             Text(text = "You've had $count glasses.")
+        }
+        Row(Modifier.padding(top = 8.dp)) {
+            Button(onClick = { count++ }) {
+                Text("Add one")
+            }
+            Button(onClick = { count = 0 }, Modifier.padding(start = 8.dp)) {
+                Text("Clear water count")
+            }
         }
     }
 }
